4efc1297ac62f0242635a7f83033d8543cb88ce6
Patrick White <whitep@vcu.edu>
2013-09-01 10:43:25 -0400

    js test files

--- includes/nav.php
+++ includes/nav.php
@@ -4,5 +4,5 @@
 	<a href="about.php">About</a>
 	<a href="portfolio.php">Portfolio</a>
 	<a href="contact.php">Contact</a>
-
+	<a href="test.php">Test</a>
 </div> <!-- end #nav -->
---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+		</head>
+	<body>
+
+	<script src="../scripts/test.js">
+	</script>
+	</body>
+</html>
