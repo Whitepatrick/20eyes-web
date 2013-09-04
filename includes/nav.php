a2b6ca752076690a5e1821ebbb886370986e91f7
Patrick White <whitep@vcu.edu>
2013-09-03 22:14:24 -0400

    reorg 20eyes layout

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,9 @@
 <div id="nav">
 
-	<a href="index.php">Home</a>
-	<a href="about.php">About</a>
-	<a href="portfolio.php">Portfolio</a>
+	<a href="index_.php">Home</a>
+	<a href="backlog.php">Backlog</a>
+	<a href="indev.php">In Development</a>
+	<a href="active.php">Active Projects</a>
 	<a href="contact.php">Contact</a>
 	<a href="test.php">Test</a>
 </div> <!-- end #nav -->
