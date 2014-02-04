b2cb2050d7c062bf628a27a4279d6493b0f0b68d
Patrick Desktop <whitep@vcu.edu>
2014-02-03 21:07:39 -0500

    added stuff to progress

--- progress.php
+++ progress.php
@@ -34,15 +34,23 @@
 
 <h1>Work In Progress</h1>
 
-<h2>CURRENT: Back end work</h2>
+<h2>CURRENT: MongoDC + PHP</h2>
+
+<p>After looking over several different login scripts for MongoDB
+in PHP I finally started developing one of my own to be custom fit
+to 20eyes.org unique infrastructure. After watching some of the logs
+while sending some DB traffic to the server I'm VERY excited about
+what I saw. Will have this problem fixed in the next day or so.</p>
+
+
+<h2>Past:</h2>
+
+<h3>Back end work</h3>
 
 <p>Now working on a feature for the site that will allow
 the user to create an account, upload an image, and submit a
 comment with the image. All managed by mongodb, scripted
 with php. It will be a new git branch to be merged after testing.</p>
-
-
-<h2>Past:</h2>
 
 <h3> Site Creation</h3>
 
