769e7fe6205115c4a6db7c7566c999b2b4b16cd5
Patrick Desktop <whitep@vcu.edu>
2014-02-13 15:13:54 -0500

    adding image

--- progress.php
+++ progress.php
@@ -34,9 +34,25 @@
 
 <h1>Work In Progress</h1>
 
-<p>test test check check</p>
+<h2>CURRENT: Jenkins-CI Installation and configuration</h2>
 
-<h2>CURRENT: MongoDB + PHP</h2>
+<a href="http://jenkins-ci.org/" target="_blank"><img src="images/jenkins.png"></a> 
+
+<p>Put the MongoDB work off for another week (it's actually not 
+supposed to be started until later in the schedule but I was getting antsy
+to try figuring out the PHP funtions for it). This week I installed <a href="http://jenkins-ci.org/a" target="_blank">Jenkins Continuous Integration</a>
+and have been working on a Build that will poll my <a href="http://www.github.com/Whitepatrick" target="_blank">GitHub</a> every
+10 minutes and if any changes are made the build is supposed to be published to my production server. However in my 
+experience nothing ever works the first time so I will keep plugging away.</p>
+
+<p><b>Good signs from Jenkins:</b> While monitoring all the logs, everything is working except the actual transfer of the files 
+which is probably the most important step in the build process BUT at least all other processes are going off without
+a hitch. SSH connection is smooth, Jenkins is connecting to GitHub and getting polling accurately. I'm still not sure if the scheduler
+is working as it should but I can sort that detail out after I start getting consistant successful builds.</p>
+
+<h2>Past:</h2>
+
+<h3>MongoDB + PHP</h3>
 
 <p>After looking over several different login scripts for MongoDB
 in PHP I finally started developing one of my own to be custom fit
@@ -47,8 +63,6 @@ what I saw. Will have this problem fixed in the next day or so.</p>
 enter username and pw on main page and script enters that info in to
 mongodb collection. Next step is write script to authenticate user after
 information entry</p>
-
-<h2>Past:</h2>
 
 <h3>Back end work</h3>
 
