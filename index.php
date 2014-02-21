c5517a3c46bc7642343678bf3eb01f947f8f38ad
Patrick Desktop <whitep@vcu.edu>
2014-02-21 12:11:55 -0500

    testing jenkins AGAIN

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<!-- test test -->
+
 
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -24,6 +24,8 @@
 </head>
 
         <body>
+
+<!-- test test -->
 
                 <div id="wrapper">
 
