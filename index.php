485c1cb998ca2b5884576154ba69d9179f907989
Patrick Desktop <whitep@vcu.edu>
2014-02-21 15:41:44 -0500

    fixing some spelling mistakes and a broken img

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 <div id="content">
 
 <h1>20 Eyes dot Org</h1>
-<h3>Exporation into the World of Contiuous Deployment</h3>
+<h3>Exploration into the World of Continuous Deployment</h3>
 
 <p>This site is contains all information and documentation on my independent study project during my last semester at Virginia Commonwealth University. My goal is to create a continuous deployment environment between 2 nodes. Along the way I hope to enhance my knowledge of front and backend web technologies. I offer this page up to prospective employers as a window to things I have learned in past job experiences, as well as knowledge gained while studying at VCU.</p> 
 
