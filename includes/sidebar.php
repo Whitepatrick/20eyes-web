5199d846d055e52e6186362b9cf7a60d10694ba7
zaphod <whitep@vcu.edu>
2015-01-03 14:02:57 -0500

    changed twitter link for new handle

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,7 @@
 
 <h3>Social Media</h3>
 	<p>
-<a href="https://twitter.com/twentyeyesorg" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @twentyeyesorg</a>
+<a href="https://twitter.com/operations_ivy" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @operations_ivy</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 </p>
 
