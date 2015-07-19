44584b69d5754bb1b9c371ec83ddd474a15d5945
Patrick White <whitep@vcu.edu>
2015-07-19 17:15:18 -0400

    adding some stuff for the day

---
+++ about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<link rel="Shortcut Icon" href="eye.ico">
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="20 Eyes Contact" />
+
+<meta name="keywords" content="contact" />
+
+<meta name="author" content="Patrick White" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen"
+/>
+
+<title>20 Eyes Dot Org</title>
+
+</head>
+
+        <body>
+
+                <div id="wrapper">
+
+
+<?php include('includes/header.php'); ?>
+
+<?php include('includes/nav.php'); ?>
+
+<?php include('includes/sidebar.php'); ?>
+
+<h1>Hi! My name is Patrick and I'm a nerd.</h1>
+<p>
+  <img src="images/jenkinsconf.jpg" class=".banner-image">
+</p>
+<p>
+  Jenkins and I at Jenkins User Conference 2015
+</p>
+<h3>
+  I live in Brooklyn, NY with my partner, <a href="https://twitter.com/liannawoods", target="_blank">Lianna</a> and our cat. I'm a software test engineer working for a medium sized content delivery company based in Washington, DC. I grew
+up in Maryland, and lived in DC until 2010 when I moved to Richmond, VA, then moved back to DC in 2014, and off to Brooklyn in 2015. I hate moving!
+</h3>
+<h3>
+  I love building and maintaining test infrastructure and automating as much possible.
+</h3>
+<h3>
+  My interests include but are not limited to Linux, Ruby, reading product reviews on
+  Amazon, and basketball.
+</h3>
+<h3>
+  In another life I worked in supply chain management or logistics.
+</h3>
+<p>
+  <img src="images/banner.png" class=".banner-image">
+</p>
+<p>
+  My desk in my old DC apartment
+</p>
+<h3>
+  I'm an emotionally mature adult who cares about working in a safe, and fair professional
+  environment.
+</h3>
+
+
+<?php include('includes/footer.php'); ?>
+
+                </div>  
+
+        </body>
+
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <div id="nav">
 
 	<a href="index.php">Home</a>
-<!--	<a href="docs.php" target="_blank">Documentation</a> -->
+	<a href="about.php">About Me!</a>
 	<a href="contact.php">Contact</a>
 	<a href="http://20eyes.org:8765" target="_blank">New Website!</a>
 </div>
